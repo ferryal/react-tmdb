c8aaa34837c4c24936fac74c21b79e7fa63a045c
ferryal <ferryal.bahari@amartha.com>
2021-03-18 21:25:51 +0700

    feat: add action types & integrate API

---
+++ src/actions/movie.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import config from '../config';
+import { LISTMOVIE, MOVIEDETAIL } from './ActionTypes';
+
+function loading() {
+  return {
+    type: LISTMOVIE.LOADING,
+  };
+}
+
+function fetchSuccess(data) {
+  return {
+    type: LISTMOVIE.FETCH_SUCCESS,
+    payload: { data },
+  };
+}
+
+function fetchFailed() {
+  return {
+    type: LISTMOVIE.FETCH_FAILED,
+  };
+}
+
+function fetchSuccessDetail(data) {
+  return {
+    type: MOVIEDETAIL.FETCH_DETAIL_SUCCESS,
+    payload: { data },
+  };
+}
+
+function fetchFailedDetail() {
+  return {
+    type: MOVIEDETAIL.FETCH_DETAIL_FAILED,
+  };
+}
+
+export function fetchListMovie(page) {
+  return (dispatch) => {
+    dispatch(loading());
+    axios.get(`${config.apiUrl}/movie/top_rated?api_key=${config.key}&page=${page}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        const response = res.data;
+        dispatch(fetchSuccess(response));
+      } else {
+        dispatch(fetchFailed());
+      }
+    }).catch(() => {
+      dispatch(fetchFailed());
+    });
+  };
+}
+
+export function fetchMovieDetail(id) {
+  return (dispatch) => {
+    axios.get(`${config.apiUrl}/movie/${id}?api_key=${config.key}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (res.status === 200) {
+        const response = res.data;
+        dispatch(fetchSuccessDetail(response));
+      } else {
+        dispatch(fetchFailedDetail());
+      }
+    }).catch(() => {
+      dispatch(fetchFailedDetail());
+    });
+  };
+}
