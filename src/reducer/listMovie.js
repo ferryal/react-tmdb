fa9c93fb8b0b87dfa4bacd3f0ea0bfa91062e349
ferryal <ferryal.bahari@amartha.com>
2021-03-18 21:27:54 +0700

    feat: add reducer list movies

---
+++ src/reducer/listMovie.js
@@ -0,0 +1,29 @@
+import { LISTMOVIE } from '../actions/ActionTypes';
+
+const initialState = {
+  loading: false,
+  movies: {},
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case LISTMOVIE.LOADING:
+    return {
+      ...state,
+      loading: true,
+    };
+  case LISTMOVIE.FETCH_SUCCESS:
+    return {
+      ...state,
+      loading: false,
+      movies: action.payload.data.results,
+    };
+  case LISTMOVIE.FETCH_FAILED:
+    return {
+      ...state,
+      loading: false,
+    };
+  default:
+    return state;
+  }
+};
