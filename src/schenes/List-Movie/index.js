9b5097c58c964f5b3ef9a19c7e504ed6201bb26e
ferryal <ferryal.bahari@amartha.com>
2021-03-19 10:14:35 +0700

    feat: integrate search & filter by year

--- src/schenes/List-Movie/index.js
+++ src/schenes/List-Movie/index.js
@@ -2,18 +2,34 @@
 import React, { useEffect, useState } from 'react';
 import { jsx, css } from '@emotion/react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchListMovie } from '../../actions/movie';
 import {
-  Navbar, Layout, Card, Loading, Button,
+  fetchListMovie, fetchSearchMovie, resetMovies, fetchFilterMovieByYear,
+} from '../../actions/movie';
+import {
+  Navbar, Layout, Card, Loading, Button, SearchBar,
 } from '../../components';
 
-const ListMovie = () => {
+const ListMovie = ({ isSearchable = true, kindURL = 'multi' }) => {
+  const thisYear = new Date().getFullYear();
+  const options = [];
+  const minOffset = 0;
+  const maxOffset = 60;
   const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [selectedYear, setselectedYear] = useState(thisYear);
+  const [isFilter, setIsFilter] = useState(false);
   const dispatch = useDispatch();
   const list = useSelector((state) => state.listMovie);
   const { movies } = list;
   const loading = false;
+
+  if (Object.keys(items).length !== 0) {
+    for (let i = minOffset; i <= maxOffset; i += 1) {
+      const year = thisYear - i;
+      options.push(<option value={year} key={year}>{year}</option>);
+    }
+  }
 
   useEffect(() => {
     dispatch(fetchListMovie(page));
@@ -24,14 +40,46 @@ const ListMovie = () => {
       setPage(page + 1);
       setTimeout(() => {
         setItems(items.concat(movies));
-      }, 1500);
+      }, 1000);
     }
   }, [movies]);
 
   const handleNextResult = () => {
-    setTimeout(() => {
+    if (search === '' && isFilter === false) {
+      setTimeout(() => {
+        dispatch(fetchListMovie(page));
+      }, 1000);
+    } else if (search !== '') {
+      setTimeout(() => {
+        dispatch(fetchSearchMovie(search, page));
+      }, 1000);
+    } else if (isFilter === true) {
+      setTimeout(() => {
+        dispatch(fetchFilterMovieByYear(selectedYear, page));
+      }, 1000);
+    }
+  };
+
+  const searchMovie = () => {
+    if (search !== '' && search.length >= 1) {
+      dispatch(resetMovies());
+      setItems([]);
+      dispatch(fetchSearchMovie(search, page));
+    } else {
+      setPage(1);
+      dispatch(resetMovies());
+      setItems([]);
       dispatch(fetchListMovie(page));
-    }, 1000);
+    }
+  };
+
+  const handleFilter = (e) => {
+    setselectedYear(e.target.value);
+    setIsFilter(true);
+    setItems([]);
+    dispatch(resetMovies());
+    setPage(1);
+    dispatch(fetchFilterMovieByYear(e.target.value, page));
   };
 
   const renderListMovie = () => (
@@ -71,7 +119,16 @@ const ListMovie = () => {
           margin-left: 3rem;
           margin-right: 3rem;
         `}
-        />
+        >
+          {isSearchable && (
+            <SearchBar
+              value={search}
+              onChange={(e) => { setSearch(e.target.value); setPage(1); }}
+              kindURL={kindURL}
+              onKeyUp={searchMovie}
+            />
+          )}
+        </div>
         <div css={css`
           display: flex;
           justify-content: center;
@@ -80,6 +137,23 @@ const ListMovie = () => {
           max-width: 100vw;
         `}
         >
+          <div
+            css={css`
+              margin-top: 4rem;
+            `}
+          >
+            {Object.keys(items).length !== 0 && (
+              <select
+                css={css`
+              z-index: 10;
+            `}
+                value={selectedYear}
+                onChange={handleFilter}
+              >
+                {options}
+              </select>
+            )}
+          </div>
           { Object.keys(movies).length !== 0 && Object.keys(items).length !== 0 ? renderListMovie() : <Loading />}
         </div>
         {loading && <Loading />}
